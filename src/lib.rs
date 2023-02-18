4c798d299f0773e9c66c660b2cdc6f53ce49f888
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-19 00:16:04 +0200

    refactor user input

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,30 @@
-use std::io;
-use std::path::PathBuf;
-
+mod error;
 mod filesystem_walker;
 mod rust_file_concatenator;
-mod error;
+mod user_input;
 use filesystem_walker::{FileSystemEntryResult, FileSystemWalker};
 
 use rust_file_concatenator::RustFileConcatenator;
+use user_input::UserInput;
 
 use crate::error::ProgramError;
 
-fn get_total_num_files() -> usize {
-    let mut input = String::new();
-    loop {
-        println!("Enter the total number of files:");
-        match std::io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                if let Ok(num) = input.trim().parse::<usize>() {
-                    return num;
-                } else {
-                    println!("Invalid input, please enter a number.");
-                    input.clear();
-                }
-            }
-            Err(_) => {
-                println!("Error reading input.");
-                input.clear();
-            }
-        }
-    }
-}
-
 pub fn run() -> FileSystemEntryResult<()> {
-    // Get the input directory and output file paths from command-line arguments
-    println!("Enter a path:");
-
-    // Read the input path from the user
-    let mut input_path = String::new();
-    io::stdin()
-        .read_line(&mut input_path)
-        .map_err(|e| ProgramError::IoError(e.into()))?;
-
-    // Remove any leading or trailing whitespace from the input path
-    let input_path = input_path.trim();
-
-    // Create a PathBuf from the input path
-    let input_dir_path = PathBuf::from(input_path);
-
-    if !input_dir_path.exists() {
-        return Err(ProgramError::NotFound);
-    }
-
-    if !input_dir_path.is_dir() {
-        return Err(ProgramError::NotADirectory);
-    }
+    let user_input = UserInput::new()?;
 
     // Create a PathBuf for the output file
     let output_file_path = std::env::current_dir()
         .map_err(|e| ProgramError::IoError(e.into()))?
         .join("output.rs");
 
-    let total_num_files = get_total_num_files();
     // Create the output file
-    let mut file_concatenator = RustFileConcatenator::new(&output_file_path, total_num_files);
+    let mut file_concatenator =
+        RustFileConcatenator::new(&output_file_path, user_input.total_files);
     file_concatenator.open_output_file()?;
 
     // Walk the directory tree and concatenate the Rust files
     let mut file_system_walker = FileSystemWalker::new(&mut file_concatenator);
-    file_system_walker.walk_directory(&input_dir_path)?;
+    file_system_walker.walk_directory(&user_input.path)?;
 
     // Close the output file
     file_concatenator.close_output_file()?;
---
+++ src/user_input.rs
@@ -0,0 +1,64 @@
+use std::{io, path::PathBuf};
+
+use crate::error::ProgramError;
+
+// Define a struct to handle user input
+pub struct UserInput {
+    pub path: PathBuf,
+    pub total_files: usize,
+}
+
+impl UserInput {
+    // Create a new UserInput object by reading user input from the console
+    pub fn new() -> Result<Self, ProgramError> {
+        let path = UserInput::get_input_path()?;
+        let total_files = UserInput::get_total_num_files()?;
+        Ok(UserInput { path, total_files })
+    }
+
+    // Prompt the user to enter the path to the input directory
+    pub fn get_input_path() -> Result<PathBuf, ProgramError> {
+        // Get the input directory and output file paths from command-line arguments
+        println!("Enter a path:");
+
+        let mut input_path = String::new();
+
+        // Read the input path from the user
+        io::stdin()
+            .read_line(&mut input_path)
+            .map_err(|e| ProgramError::IoError(e.into()))?;
+        // Remove any leading or trailing whitespace from the input path
+        let input_path = input_path.trim();
+
+        // Create a PathBuf from the input path
+        let input_dir_path = PathBuf::from(input_path);
+
+        if !input_dir_path.exists() {
+            return Err(ProgramError::NotFound.into());
+        }
+
+        if !input_dir_path.is_dir() {
+            return Err(ProgramError::NotADirectory.into());
+        }
+        
+        Ok(input_dir_path)
+    }
+
+    // Prompt the user to enter the total number of files to concatenate
+    pub fn get_total_num_files() -> Result<usize, ProgramError> {
+        let mut input = String::new();
+        loop {
+            println!("Enter the total number of files:");
+            io::stdin()
+                .read_line(&mut input)
+                .map_err(|e| ProgramError::IoError(e.into()))?;
+            
+            if let Ok(num) = input.trim().parse::<usize>() {
+                return Ok(num);
+            } else {
+                println!("Invalid input, please enter a number.");
+                input.clear();
+            }
+        }
+    }
+}
